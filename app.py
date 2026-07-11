8208e8260aa1277384796f1e5069c59a3c3fc527
visheshsatarkar15 <visheshsatarkar7179@gmail.com>
2026-07-11 23:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
-import os
-from google import genai
 
-app = FastAPI(title="IRIS Server v1.0")
+from routes import router
 
-from config import Config
-
-client = genai.Client(
-    api_key=Config.GEMINI_API_KEY
+app = FastAPI(
+    title="IRIS Server v1.0",
+    version="1.0"
 )
+
 
 @app.get("/")
 def root():
+
     return {
         "status": "online",
         "name": "IRIS Server v1.0",
         "message": "IRIS Cloud Brain is running."
     }
+
+
+app.include_router(router)
